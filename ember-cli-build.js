1492a0477a5eb0e316b85284dceba261d7c9508f
thefonso <thefonso@thefonso.com>
2017-07-01 15:31:30 -0700

    sounds can load on main screen

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -6,6 +6,8 @@ module.exports = function(defaults) {
     // Add options here
   });
 
+  app.import('bower_components/howler.js/dist/howler.core.min.js');
+
   // Use `app.import` to add additional libraries to the generated
   // output files.
   //
---
+++ walkthrough.md
@@ -0,0 +1,19 @@
+## install howler
+
+npm install -g bower
+
+bower install yarn --save
+
+```
+ember-cli-build.js
+
+...
+app.import('bower_components/howler.js/dist/howler.core.min.js');
+...
+
+```
+
+```
+jshintrc
+
+howel
